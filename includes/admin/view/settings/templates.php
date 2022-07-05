09e20b3fae54f1e267193ff261446be466ce8eaf
Tomizawa Kan <sandman.jp@gmail.com>
2022-07-05 04:16:51 +0000

    追加CSSフィールドの追加

--- includes/admin/view/settings/templates.php
+++ includes/admin/view/settings/templates.php
@@ -8,6 +8,62 @@ $templates = $this->settings->get('templates-settings');
 $calendar_type = empty($templates['calendar-type']) ? 'monthly' : $templates['calendar-type'];
 $column_header = empty($templates['column-header']) ? false : $templates['column-header'];
 $seamless_month = empty($templates['seamless-month']) ? false : $templates['seamless-month'];
+ob_start();
+?>
+.cfc-table {
+	width: 100%;
+	border: 1px solid #9e9889;
+	border-collapse: collapse; }
+	.cfc-table th, .cfc-table td {
+		font-size: 14px;
+		width: calc(100% / 7);
+		border-left: 1px solid #dcd7ca; }
+	.cfc-table.have_column_header th, .cfc-table.have_column_header td {
+		width: calc(100% / 8); }
+	.cfc-table td {
+		position: relative;
+		vertical-align: top; }
+	.cfc-table time {
+		display: -webkit-box;
+		display: -ms-flexbox;
+		display: flex;
+		width: 30px;
+		height: 30px;
+		font-size: 12px;
+		font-weight: bold;
+		-webkit-box-align: center;
+				-ms-flex-align: center;
+						align-items: center;
+		-webkit-box-pack: center;
+				-ms-flex-pack: center;
+						justify-content: center;
+		position: absolute;
+		top: 0;
+		right: 0;
+		color: #fff;
+		background: #9e9889; }
+	.cfc-table .cfc-item {
+		font-weight: bold;
+		color: #9e9889; }
+	.cfc-table .cfc-data, .cfc-table .cfc-item {
+		margin: 0;
+		padding: 5px;
+		font-size: 14px;
+		border-bottom: 1px solid #dcd7ca; }
+		.cfc-table .cfc-data:last-of-type, .cfc-table .cfc-item:last-of-type {
+			border: none; }
+		.cfc-table .cfc-data:first-of-type, .cfc-table .cfc-item:first-of-type {
+			border-bottom: 1px solid #dcd7ca; }
+
+.cfc-table-weekly td {
+	padding-top: 32px; }
+.cfc-table-weekly time {
+	width: 100%; }
+.cfc-table-weekly .cfc-items {
+	padding-top: 32px; }
+<?php ;
+$add_css = ob_get_clean();
+$add_css = !empty($templates['add-css']) ? $templates['add-css'] : $add_css;
 ?>
 <div class="p-front-view_tab">
 <div class="c-fieldset cfc-<?php echo $calendar_type ?>">
@@ -43,6 +99,15 @@ $seamless_month = empty($templates['seamless-month']) ? false : $templates['seam
 			<label>
 				<input type="checkbox" name="templates-settings[column-header]" value="1" <?php checked( $column_header, 1); ?>> <?php _e('Show column header', CFC_TEXTDOMAIN); ?>
 			</label>
+		</div>
+		
+	</div>
+	
+	<div class="c-field add-css">
+		<div class="c-field_label"><?php _e('Additional Stylesheet', CFC_TEXTDOMAIN); ?></div>
+		
+		<div class="c-field_input"">
+			<textarea name="templates-settings[add-css]" rows="10"><?php echo $add_css; ?></textarea>
 		</div>
 		
 	</div>
--- includes/shortcode.php
+++ includes/shortcode.php
@@ -13,6 +13,8 @@ class shortcode{
 	
 	function __construct(){
 		add_shortcode('cfc', array($this, 'do_shortcode'));
+		
+		add_action('wp_enqueue_scripts', array($this, 'enqueue_styles'));
 	}
 	
 	function do_shortcode($atts){
@@ -22,6 +24,28 @@ class shortcode{
 		echo $calendar->rendar_table($atts);
 		return ob_get_clean();
 		
+	}
+	
+	function enqueue_styles(){
+		global $posts;
+		require_once CFC_DIR_INCLUDES.'/settings.php';
+		
+		wp_register_style( 'cfc', false );
+		wp_enqueue_style( 'cfc' );
+		
+		$add_css = '';
+		//var_dump($posts);
+		foreach($posts as $p){
+			$cotent = $p->post_content;
+			if(preg_match("/\[cfc.+?id=\"(\d+?)\"\]/", $cotent, $match)){
+				
+				$settings = new CFC\settings($match[1]);
+				//CFC()->register_instance($this->settings);
+				$add_css .= $settings->get_stylesheet();
+				
+			}
+		}
+		wp_add_inline_style( 'cfc', $add_css );
 	}
 	
 }
