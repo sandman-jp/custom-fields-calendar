02fb8e16fcf341e08ef333b24dd15334496e419b
Tomizawa Kan <sandman.jp@gmail.com>
2022-07-15 12:46:16 +0000

    1.3.1-alpha

--- custom-fields-calendar.php
+++ custom-fields-calendar.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Fields Calendar
 Plugin URI: 
 Description: Calendar with custom fields.
-Version: 1.3.0-alpha
+Version: 1.3.1-alpha
 Author: 地空 Chhkuw Design
 Author URI: 
 Text Domain: cf_calendar
